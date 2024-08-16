96f2cd806e3b08fd7a07f120d75968612a582d52
Elsa Villarreal <elsa22@att.net>
2024-08-16 11:36:23 -0500

    complete

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car toyotaRav4 = new Car();
+
+            toyotaRav4.Make = "Toyota";
+            toyotaRav4.Model = "Rav 4";
+            toyotaRav4.Year = 2024;
+
+            Console.WriteLine($"You own a {toyotaRav4.Year} {toyotaRav4.Make} {toyotaRav4.Model}");
+
         }
     }
 }
